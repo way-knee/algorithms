98ed2b1a96b7c60d279746bc5d0f4e7bbb50a1c5
way-knee <winnie.chen.chouchou@gmail.com>
2022-10-02 21:02:22 -0400

    add bubble sort algorithm

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+def bubble_sort(A):
+    n = len(A)
+    i = 0
+    flag = 0
+    while i < n-1:
+        j = 0
+        while j < n-i-1:
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                flag = 1
+            j += 1
+        i += 1
+        if flag == 0:
+            return A
+            break
+    return A
